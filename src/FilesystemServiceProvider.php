98f45b50a08b9f9de4a7d9d92031e1eef7e0ce71
wanganlin <inbox@wanganlin.com>
2023-06-15 10:18:52 +0800

    upgrade

---
+++ src/FilesystemServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laractl\Filesystem;
+
+use Illuminate\Support\ServiceProvider;
+
+class FilesystemServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
